bef160d8bf3467b9f4aeba7d67c16bae3b2d2e9d
RobertToh <35612823+RobertToh@users.noreply.github.com>
2021-06-15 11:13:41 -0600

    fix profile picture not centered

--- src/ProfileInfo.js
+++ src/ProfileInfo.js
@@ -9,18 +9,9 @@ class ProfileInfo extends React.Component {
     render() {
         let profile = this.props.profile;
         return (
-        // <div className="justify-content-md-center">
-        //     <div className="col-auto">
-        //         <img src={profile.avatar} alt="avatar" className="border border-dark border-3 rounded"/>
-        //     </div>
-        //     <div className="col-auto">
-        //         <span><h3>{profile.personaname}</h3></span>
-        //     </div>
-        // </div>
             <div className="card " style={{width: "14rem"}}>
                 <img src={profile.avatar} alt="avatar" className="card-img-top border border-dark border-3 rounded" />
                 <div className="card-body py-2">
-                    {/* <span><h5>{profile.personaname}</h5></span> */}
                     <h5 className="card-subtitle">{profile.personaname}</h5>
                 </div>
             </div>
